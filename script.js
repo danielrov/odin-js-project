7aa982467c7c51b8f90c97a4382da5a2fa5c51ae
danielrov <80066620+danielrov@users.noreply.github.com>
2024-06-14 11:43:13 -0300

    feat(game): add rock paper scissors game
    Add a JavaScript file that implements the rock paper scissors
    game. This initial version includes basic gameplay logic for
    player vs. computer matches.

---
+++ script.js
@@ -0,0 +1,62 @@
+let computerChoice
+let humanChoice
+let humanScore = 0
+let computerScore = 0
+
+function getComputerChoice(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    let randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+    if (randomNumber === 1) {
+        computerChoice = "Rock"
+    } else if (randomNumber === 2) {
+        computerChoice = "Paper"
+    } else {
+        computerChoice = "Scissors"
+    }
+}
+
+function getHumanChoice(){
+    let humanNumber = +prompt("What is your choice? (Between 1 and 3)?")
+    if (humanNumber === 1) {
+        humanChoice = "Rock"
+    } else if (humanNumber === 2) {
+        humanChoice = "Paper"
+    } else if (humanNumber === 3) {
+        humanChoice = "Scissors"
+    } else {
+        humanChoice = "Please choose a valid number."
+    }
+}
+
+function playRound() {
+    getComputerChoice(1,3)
+    getHumanChoice()
+    if (humanChoice === computerChoice) {
+        result = "That's a tie"
+    } else if ((computerChoice === "Rock" && humanChoice === "Scissors") || (computerChoice === "Paper" && humanChoice === "Rock") || (computerChoice === "Scissors" && humanChoice === "Paper")) {
+        result = "You lose. " + computerChoice + " wins " + humanChoice;
+        computerScore = ++computerScore;
+    } else {
+        result = "You win. " + humanChoice + " wins " + computerChoice;
+        humanScore = ++humanScore;
+    }
+    console.log(result)
+}
+
+function playGame(){
+    let games = 0
+    while (games < 5){
+        ++games;
+        playRound(humanChoice,computerChoice)
+        console.log("Human Score is: " + humanScore)
+        console.log("Computer Score is: " + computerScore)
+    }
+}
+
+playGame()
+
+
+/* 
+
+*/
